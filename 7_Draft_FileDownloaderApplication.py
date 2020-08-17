44740029875d44d6851699e4ce7c43627a9ea26a
RS-codes <RS-codes@github.com>
2020-08-17 09:54:26 +0530

    added 7_Draft_FileDownloaderAppication.py

---
+++ 7_Draft_FileDownloaderApplication.py
@@ -0,0 +1,15 @@
+# Step1: write down our requirements
+# Requirements:
+# 1. Download a file from URL found in QlineEdit
+# 2. Display a QProgressBar while downloading file
+# 3. Allow the user to select save location using "File Save" dialog
+# 4. Display a message when download is complete
+# 5. Handle errors(exceptions) by displaying an error message
+#
+# Step2: Draw
+# Sample figure of our UI:
+#
+# Step3: Development Steps
+# 1. Basic UI
+# 2. Basic functionality
+# 3. Extended functionality
